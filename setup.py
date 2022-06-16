328036c716aae0a5cfabc925a7022ab8635fe300
landmanbester <lbester@ska.ac.za>
2022-06-16 13:50:35 +0200

    Restructure image convolver

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import pfb
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+requirements = [
+                'omegaconf',
+                'katbeam',
+                'pytest >= 6.2.2',
+                'pyscilog >= 0.1.2',
+                'codex-africanus[complete]',
+            ]
+
+
+setup(
+     name='spimple',
+     version=spimple.__version__,
+     author="Landman Bester",
+     author_email="lbester@sarao.ac.za",
+     description="Radio astronomy image post-processing tools",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/landmanbester/spimple",
+     packages=find_packages(),
+     python_requires='>=3.7',
+     install_requires=requirements,
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+     entry_points='''
+                    [console_scripts]
+                    imconv=apps.image_convolver:image_convolver
+     '''
+     ,
+ )
---
+++ spimple/__init__.py
@@ -0,0 +1,10 @@
+"""
+Radio astronomy image post-processing tools
+
+author - Landman Bester
+email  - lbester@sarao.ac.za
+date   - 16/06/2022
+"""
+
+__version__='0.0.1'
+
