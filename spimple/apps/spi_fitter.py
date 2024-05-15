d50fde4516527b3c28d7a92e8f59f45ef8c4bc6d
Athanaseus Javas Ramaila <aramaila@ska.ac.za>
2024-05-15 08:44:17 +0200

    typo

--- spimple/apps/spi_fitter.py
+++ spimple/apps/spi_fitter.py
@@ -260,7 +260,7 @@ def spi_fitter():
             else:
                 raise ValueError(f"Unknown beam model for katbeam in band {opts.band}")
             for v in range(freqs.size):
-                beam_imageo.append(beam.I(xx, yy, freqs[v]/1e6))  # freqs in MHz
+                beam_image.append(beam.I(xx, yy, freqs[v]/1e6))  # freqs in MHz
             beam_image = np.stack(beam_image)
 
         else:
